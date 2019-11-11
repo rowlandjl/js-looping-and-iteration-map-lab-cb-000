b80a832fcff611feb68ea75455c37a1acde5bd1e
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 16:37:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+
+
+function nameToAttributes(array) {
+  return array.map(function(driver) {
+    const driverFirstName = driver.split(' ')[0];
+    const driverLastName = driver.split(' ')[1];
+    
+    return { firstName: driverFirstName, lastName: driverLastName };
+  });
+}
+
+
+
+function attributesToPhrase(array) {
+  return array.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
+
+
+
