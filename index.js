1ba8ec924a56274d586776192e07815e621ea234
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 16:37:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function nameToAttributes(array) {
 
 function attributesToPhrase(array) {
   return array.map(function(driver) {
-    return `${driver.name} is from ${driver.hometown}
-  })
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
 
 
